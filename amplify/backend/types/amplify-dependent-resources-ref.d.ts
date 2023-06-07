75e8e4710fc82e3461a4557757add42c06384410
StasSklyar <stas.sklyar.0000@gmail.com>
2023-06-07 17:40:19 +0300

    added S3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s34781ae10": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
